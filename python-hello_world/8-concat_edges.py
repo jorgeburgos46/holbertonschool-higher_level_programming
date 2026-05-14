86727f35139e2720f0834b388ec8857ece4b26ad
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-05-14 14:31:49 -0400

    Fix concat using only slicing without string literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str1 = "Python is an interpreted, interactive, object-oriented programming language"
-str2 = "that is easy to learn"
-print(str1[39:] + " with " + str1[:6])
+str2 = "Yuck!"
+print(str1[39:74] + str2[1:6] + str1[:6])
