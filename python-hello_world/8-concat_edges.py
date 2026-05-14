cd1569c8e3cd64b07b4c834d087e6ac7c1a0aa67
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-05-14 14:35:30 -0400

    Use string slicing to print OOP sentence without string literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming language"
-str2 = "Yuck!"
-print(str1[39:74] + str2[1:6] + str1[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[107:112] + str[0:6])
