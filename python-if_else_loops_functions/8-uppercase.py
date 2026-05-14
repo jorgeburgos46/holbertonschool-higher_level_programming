0780ef15f18202a71d6ae5927712a48eaffb1589
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-05-14 14:54:31 -0400

    Fix uppercase to build result string and print once

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
-            print(chr(ord(c) - 32), end="")
+            result += chr(ord(c) - 32)
         else:
-            print(c, end="")
-    print("")
+            result += c
+    print("{}".format(result))
