36f7bc642748697a89daec75a6c886bedde97a70
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-05-22 18:52:31 -0400

    Add Square class with __str__ method for printing

---
+++ python-classes/101-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""This module defines a Square class with str representation."""
+
+
+class Square:
+    """A class that defines a square with size and position attributes."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes a Square with a given size and position."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square with type and value validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets position with validation for tuple of 2 positive ints."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not isinstance(value[0], int) or
+                not isinstance(value[1], int) or
+                value[0] < 0 or value[1] < 0):
+            raise TypeError(
+                "position must be a tuple of 2 positive integer")
+        self.__position = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with # using position offsets."""
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(self.__position[1]):
+            print("")
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Returns string representation same as my_print."""
+        if self.__size == 0:
+            return ""
+        result = ""
+        for i in range(self.__position[1]):
+            result += "\n"
+        for i in range(self.__size):
+            result += " " * self.__position[0] + "#" * self.__size
+            if i < self.__size - 1:
+                result += "\n"
+        return result
