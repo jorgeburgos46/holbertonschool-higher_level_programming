ca855ee2186d7ab5eb97f7847cab3442f747f89f
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-05-22 16:10:24 -0400

    Documentation added in the code with the Pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module definess a square class with size validation."""
 class Square:
+    """A class that define a square with a private size attribute."""
     def __init__(self):
-
+        """Initializes a Square with a given size."""
