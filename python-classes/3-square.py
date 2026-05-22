8ff24b08f93f6029c233807fb7d1838e53f85976
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-05-22 18:11:53 -0400

    Fix indentation in Square class with area method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a Square class with calidation and area method."""
+"""This module defines a Square class with size validation and area method."""
+
 
 class Square:
-    """ A class that define a square with a private size attribute."""
+    """A class that defines a square with a private size attribute."""
 
-        def __init__(self, size=0):
-            """Initializes a Square with a given size, default is 0."""
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be an integer")
-            self.__size = size
+    def __init__(self, size=0):
+        """Initializes a Square with a given size, default is 0."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-            def area(self):
-                """Return the current square area."""
-                return self.__size ** 2
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
