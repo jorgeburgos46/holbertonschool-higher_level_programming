ce5fb55f1eeaa595bc917ea357edd5e3c0f1bbd0
jorgeburgos46 <jorgeburgos46@gmail.com>
2026-05-22 18:39:33 -0400

    Add Square class with property getter and setter for size

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,28 +2,27 @@
 """This module defines a Square class with getter and setter for size."""
 
 
-class square:
+class Square:
     """A class that defines a square with a private size attribute."""
 
     def __init__(self, size=0):
         """Initializes a Square with a given size, default is 0."""
         self.size = size
 
-        @property
-        def size(self):
-            """Retrives the size of the square."""
-            return self.__size
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
 
-        @size.setter
-        def size(self, valeu):
-            """Sets the size of the square with type and value validation."""
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square with type and value validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """Return the current square area."""
-            return self.__size ** 2
-
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
