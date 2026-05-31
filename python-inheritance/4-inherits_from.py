273cfe3ca3ab003915834db15b2582d55cb0d00b
Jorge <jorge@Jorges-MacBook-Pro.local>
2026-05-31 12:01:44 -0400

    Fix PEP8 docstring line length in 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,9 @@
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that is a subclass of a_class."""
+    """Return True if obj is an instance of a class that inherits from a_class.
+
+    The object must be an instance of a subclass of a_class, not a_class
+    itself.
+    """
     return type(obj) is not a_class and isinstance(obj, a_class)
