dec852aa24540abfff5fe8f5a3b52f42454d15af
Jorge <jorge@Jorges-MacBook-Pro.local>
2026-05-31 14:36:42 -0400

    Add task 11: Square with str representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with string representation."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return string description of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
