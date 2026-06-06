550313de095b1971d5d93a2030b38bc19e4ceb42
Jorge <jorge@Mac.lan>
2026-06-06 17:56:03 -0400

    a class Student that defines a student by: (based on 10-student.py)

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define a Student class with JSON serialization and reload support."""
+
+
+class Student:
+    """Represent a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student instance."""
+        obj_dict = self.__dict__.copy()
+        if isinstance(attrs, list):
+            filtered = {}
+            for key in attrs:
+                if isinstance(key, str) and key in obj_dict:
+                    filtered[key] = obj_dict[key]
+            return filtered
+        return obj_dict
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance from json dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
