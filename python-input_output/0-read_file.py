133e617bbeedc33dda55fbb6b831d7cf3fbef579
Jorge <jorge@Mac.lan>
2026-06-06 17:22:12 -0400

    Error fixed in the line 5

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,7 +2,11 @@
 """This module reads the content of a file and prints it to the console."""
 
 
-with open('my_file_object.txt') as file:
-    """Reads the content of 'my_file_object.txt' and prints it."""
-    print(file.read())
-    """Note: The file is automatically closed after the block is executed due to the use of 'with' statement."""
+def read_file(filename=""):
+    """Read and print the content of a file."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
+
+
+if __name__ == "__main__":
+    read_file("my_file_object.txt")
