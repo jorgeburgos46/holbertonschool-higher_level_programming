8ccfa32a8943f773fb436551e7b19076652e4e40
Jorge <jorge@Mac.lan>
2026-06-06 17:32:30 -0400

    Add write_file implementation to python-input_output/1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write text to a UTF-8 encoded file."""
+
+
+def write_file(filename="", text=""):
+    """Write text to filename using UTF-8 and return the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
