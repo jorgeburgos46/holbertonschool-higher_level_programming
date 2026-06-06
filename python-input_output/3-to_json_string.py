f83acb5db757bfa368a0b4fe900ec3bd54d21437
Jorge <jorge@Mac.lan>
2026-06-06 17:38:19 -0400

    error in the push

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Return the JSON representation of an object as a string."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of my_obj."""
+    return json.dumps(my_obj)
