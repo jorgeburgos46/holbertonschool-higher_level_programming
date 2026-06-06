d32784d9e7c55547597403ea193d5a0ed31830e8
Jorge <jorge@Mac.lan>
2026-06-06 17:34:15 -0400

    Fixed pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,9 @@
 
 
 def write_file(filename="", text=""):
-    """Write text to filename using UTF-8 and return the number of characters written."""
+    """Write text to filename using UTF-8.
+
+    Return the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
